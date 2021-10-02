3de8fe08a46234d5af6454a22205fb367d2610a1
William Redman <william.redman1989@gmail.com>
2021-10-02 12:46:25 +0100

    add another test

--- tests/OpenApiGeneratorTest.php
+++ tests/OpenApiGeneratorTest.php
@@ -98,4 +98,92 @@ JSON,
             $json
         );
     }
+
+    public function testApiWithPathParamsAndMultipleMethodsPerPath(): void
+    {
+        $data = <<<DATA
+Account
+- id: int
+- name: string
+
+GET /account/{id} => Account
+DELETE /account/{id}
+DATA;
+        $result = (new Parser())->parse($data);
+
+        $openApi = (new OpenApiGenerator('PathParamsAndMultiMethod', '0.0.2', $result))->generate();
+        file_put_contents(
+            __DIR__ . '/../output/openapi-path-params-and-multi-method.json',
+            $json = json_encode($data = $openApi->getSerializableData(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+        );
+        $this->assertSame('PathParamsAndMultiMethod', $data->info->title);
+        $this->assertSame('0.0.2', $data->info->version);
+
+        $this->assertJsonStringEqualsJsonString(
+            <<<JSON
+{
+    "openapi": "3.0.0",
+    "info": {
+        "title": "PathParamsAndMultiMethod",
+        "version": "0.0.2"
+    },
+    "paths": {
+        "/account/{id}": {
+            "parameters": [
+                {
+                    "name": "id",
+                    "in": "path",
+                    "required": true,
+                    "schema": {
+                        "type": "string"
+                    }
+                }
+            ],
+            "get": {
+                "responses": {
+                    "200": {
+                        "description": "Success",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "\$ref": "#/components/schemas/Account"
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            "delete": {
+                "responses": {
+                    "200": {
+                        "description": "Success"
+                    }
+                }
+            }
+        }
+    },
+    "components": {
+        "schemas": {
+            "Account": {
+                "required": [
+                    "id",
+                    "name"
+                ],
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "integer"
+                    },
+                    "name": {
+                        "type": "string"
+                    }
+                }
+            }
+        }
+    }
+}
+JSON,
+            $json
+        );
+    }
 }
